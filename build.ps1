a190fe34e040368422b85078c4d18836bb51215b
Douglas Shibata <44437992+douglasshibata@users.noreply.github.com>
2024-09-24 18:23:44 -0300

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,13 +1,3 @@
-# Navegar para o diretório 'desafio'
-Set-Location -Path "desafio"
-
-
-    ./mvnw clean package -DskipTests
-
-Write-Output "Clean package finished!"
-
-# Voltar para o diretório anterior
-Set-Location -Path ".."
 
 # Subir os containers com Docker Compose
 docker-compose up -d

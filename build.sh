a27a99b905416c3437b1cf0ccf9e4c988c3ef71c
Douglas Shibata <44437992+douglasshibata@users.noreply.github.com>
2024-09-24 18:00:03 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 cd desafio
-# Check for M2_HOME environment variable
-if [[ -n "$M2_HOME" ]]; then
-# mvn clean package -Pnative  -DskipTests
-  mvn clean package -DskipTests
-else
-  ./mvnw clean package -DskipTests
-fi
+ ./mvnw clean package -DskipTests
 echo "Clean package finished!"
 cd ..
 docker compose up -d
